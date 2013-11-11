5c0cd7960331eba9ae737554b90e938ef1352337
Mishail <mishail.mishail@gmail.com>
2013-11-10 22:38:11 -0800

    1st working version

--- src/main/java/com/github/cqljmeter/sampler/CqlSampler.java
+++ src/main/java/com/github/cqljmeter/sampler/CqlSampler.java
@@ -2,28 +2,23 @@ package com.github.cqljmeter.sampler;
 
 import java.util.concurrent.TimeUnit;
 
-import org.apache.jmeter.config.ConfigTestElement;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testbeans.TestBean;
-import org.apache.jmeter.testelement.TestElement;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
 public class CqlSampler extends AbstractSampler implements TestBean {
-	@Override
-	public boolean applies(ConfigTestElement configElement) {
-        String guiClass = configElement.getProperty(TestElement.GUI_CLASS).getStringValue();
-        log.warn(guiClass);
-        return "org.apache.jmeter.config.gui.SimpleConfigGui".equalsIgnoreCase(guiClass);
-	}
 
 	private static final Logger log = LoggingManager.getLoggerForClass();
 	private static final long serialVersionUID = -996507992186021290L;
 	
 	private String query = "";
+	private String queryTimeout = "";
 
+	public CqlSampler(){}
+	
 	public SampleResult sample(Entry arg0) {
 		log.warn("sampling cql: " + query);
 		SampleResult result = new SampleResult();
@@ -56,6 +51,14 @@ public class CqlSampler extends AbstractSampler implements TestBean {
 
 	public void setQuery(String query) {
 		this.query = query;
+	}
+
+	public String getQueryTimeout() {
+		return queryTimeout;
+	}
+
+	public void setQueryTimeout(String queryTimeout) {
+		this.queryTimeout = queryTimeout;
 	}
 
 }
--- src/main/java/com/github/cqljmeter/sampler/CqlSamplerBeanInfo.java
+++ src/main/java/com/github/cqljmeter/sampler/CqlSamplerBeanInfo.java
@@ -3,21 +3,21 @@ package com.github.cqljmeter.sampler;
 import java.beans.PropertyDescriptor;
 
 import org.apache.jmeter.testbeans.BeanInfoSupport;
-import org.apache.jmeter.testbeans.TestBean;
 import org.apache.jmeter.testbeans.gui.TypeEditor;
 
 public class CqlSamplerBeanInfo extends BeanInfoSupport {
-	protected CqlSamplerBeanInfo(Class<? extends TestBean> arg0) {
-		super(arg0);
-		createPropertyGroup("cql", new String[] { "query" });
-
-		PropertyDescriptor p = property("query", TypeEditor.TextAreaEditor);
-		p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-		p.setValue(DEFAULT, "");
-		p.setValue(TEXT_LANGUAGE, "sql");
-	}
 
 	public CqlSamplerBeanInfo() {
 		super(CqlSampler.class);
+		createPropertyGroup("cql", new String[] { "query", "queryTimeout" });
+		
+		PropertyDescriptor p = property("queryTimeout"); // $NON-NLS-1$
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, "");
+		
+		 p = property("query", TypeEditor.TextAreaEditor);
+		p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+		p.setValue(DEFAULT, "test");
+		p.setValue(TEXT_LANGUAGE, "sql");
 	}
 }
